185cacc8e633b4ac27ecb38750b70d824f5c274f
launderex619 <carlos12150@hotmail.com>
2019-10-26 23:56:04 -0500

    Primera pagina

--- Power/src/app/app.module.ts
+++ Power/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    
   ],
   imports: [
+    AngularMaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Power/src/interfaces/test.ts
@@ -0,0 +1,7 @@
+
+  export interface PeriodicElement {
+    id: string;
+    name: string;
+    progress: string;
+    color: string;
+  }
